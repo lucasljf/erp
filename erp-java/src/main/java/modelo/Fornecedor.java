3a0351a156d2d5580f23d72ad89b81caa17e6ef8
kaickbmc <kaick.costa@estudante.ifgoiano.edu.br>
2024-03-15 15:13:22 -0300

    Create Fornecedor.java
    Criação da classe Fornecedor, seus atributos e os métodos get e set.

---
+++ erp-java/src/main/java/modelo/Fornecedor.java
@@ -0,0 +1,83 @@
+package modelo;
+
+/**
+ *
+ * @author kaick
+ */
+public class Fornecedor {
+    private int id;
+    private String nome;
+    private String telefone;
+    private String cnpj;
+    private String email;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the cnpj
+     */
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    /**
+     * @param cnpj the cnpj to set
+     */
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
