1ce5008b9b8252eb5763f8e573d3ba2e5ea5677f
Sara Farias <fariassara012@gmail.com>
2024-03-16 21:06:17 -0300

    Criação da classe Usuario.java
    Criação da classe Usuario com atributos nome, login e senha e seus respectivos getters e setters.

---
+++ erp-java/src/main/java/modelo/Usuario.java
@@ -0,0 +1,42 @@
+
+package modelo;
+
+/**
+ *
+ * @author sara
+ */
+public class Usuario {
+    
+    private String nome;
+    private String login;
+    private String senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+    
+    
+}
