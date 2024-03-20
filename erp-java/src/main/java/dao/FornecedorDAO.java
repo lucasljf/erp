d6a1e0b71b153c452e51cbd3d7269bb9821ac254
zirilu <felipe16gomes07rosa@gmail.com>
2024-03-20 20:57:41 -0300

    Create salvar fornecedor

--- erp-java/src/main/java/dao/FornecedorDAO.java
+++ erp-java/src/main/java/dao/FornecedorDAO.java
@@ -1,6 +1,35 @@
 
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import modelo.Fornecedor;
+
 public class FornecedorDAO {
+    
+    public Fornecedor salvar(Fornecedor fornecedor){
+    
+        Conexao con = new Conexao();
+        con.getConexao();
+      
+        String sql = "INSERT INTO fornecedor (nome, telefone, cnpj, email) VALUES (?, ?, ?, ?)";
+        
+        try {
+        PreparedStatement stmt = this.con.preparedStatement(sql);
+        stmt.setString(1, fornecedor.getNome());
+        stmt.setString(2, fornecedor.getTelefone());
+        stmt.setString(3, fornecedor.getCnpj());
+        stmt.setString(4, fornecedor.getEmail());
+        
+        stmt.execute();
+        stmt.close();
+        
+        } catch (SQLException e){
+            throw new RuntimeException();
+            
+        }
+        
+        return fornecedor;
+    }
 
 }
