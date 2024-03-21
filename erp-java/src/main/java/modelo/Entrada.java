a4656bf93b586c305928bed3cf3ee685f77ec1f0
Thiago Silva <smt.thiago99@gmail.com>
2024-03-20 22:36:37 -0300

    Atualizando entrada
    Retirando os métodos não necessários

---
+++ erp-java/src/main/java/modelo/Entrada.java
@@ -0,0 +1,78 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Entrada {
+    
+    private int id;
+    private Date data;
+    private int quantidade;
+    private double precoCusto;
+    private Fornecedor fornecedor;
+    private String lote;
+    private Date validade;
+
+    public Entrada(Date data, int quantidade, double precoCusto, Fornecedor fornecedor, String lote, Date validade) {
+        this.data = data;
+        this.quantidade = quantidade;
+        this.precoCusto = precoCusto;
+        this.fornecedor = fornecedor;
+        this.lote = lote;
+        this.validade = validade;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public String getLote() {
+        return lote;
+    }
+
+    public void setLote(String lote) {
+        this.lote = lote;
+    }
+
+    public Date getValidade() {
+        return validade;
+    }
+
+    public void setValidade(Date validade) {
+        this.validade = validade;
+    }
+}
