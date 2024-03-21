ad270984f7ad390dae6598e3e97a840a94301c74
MarioALves2002 <marioalves5627@gmail.com>
2024-03-20 21:54:12 -0300

    git update Conexao.java

--- erp-java/src/main/java/dao/Conexao.java
+++ erp-java/src/main/java/dao/Conexao.java
@@ -9,7 +9,7 @@ public class Conexao {
     public Connection conectar() throws Exception {
         Class.forName("com.mysql.jdbc.Driver");
         return DriverManager.getConnection(
-                "jdbc:mysql:// localhost:3306/erpDB", 
+                "jdbc:mysql:// localhost:3306/banco_erp", 
                 "root", "123456789");
     }
 
