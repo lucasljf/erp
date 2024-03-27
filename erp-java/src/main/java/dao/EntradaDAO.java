9bf31b07d8c24a2581f2d32685464e3e64f150d7
guiAlencar <uchihaguilherme4@gmail.com>
2024-03-26 23:25:24 -0300

    ajustando entradaDao
    como existe composição por parte do fornecedor, dei uma pesquisada e achei esse modo de fazer, se não for assim, irei precisar de ajuda para finalizar essa classe

--- erp-java/src/main/java/dao/EntradaDAO.java
+++ erp-java/src/main/java/dao/EntradaDAO.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import modelo.Produto;
+import modelo.Entrada;
+import modelo.Fornecedor;
 
 
 public class EntradaDAO {
@@ -17,12 +19,22 @@ public class EntradaDAO {
         try {
             PreparedStatement stmt = this.conexao.prepareStatement(sql);
             stmt.setInt(1, produto.getId());
-               
+            
             ResultSet rs = stmt.executeQuery();
             ArrayList<Entrada> entradas = new ArrayList<>();
 
             while (rs.next()) {
                 Entrada entrada = new Entrada(rs.getInt("id"));
+                entrada.setData(rs.getDate("data"));
+                entrada.setQuantidade(rs.getInt("quantidade"));
+                entrada.setPrecoCusto(rs.getDouble("precoCusto"));
+                
+                Fornecedor fornecedor = new Fornecedor();
+                fornecedor.setId(rs.getInt("id"));
+                entrada.setFornecedor(fornecedor);
+                
+                entrada.setLote(rs.getString("lote"));
+                entrada.setValidade(rs.getDate("validade"));
                 entradas.add(entrada);
             }
             
