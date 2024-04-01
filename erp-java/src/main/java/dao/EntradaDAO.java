24fad1468dc3ff28a85713c4ef7724814fe3d2a8
Carlos Cruz <edduferr007@gmail.com>
2024-04-01 20:04:50 -0300

    Update buscar(Fornecedor) EntradaDAO

--- erp-java/src/main/java/dao/EntradaDAO.java
+++ erp-java/src/main/java/dao/EntradaDAO.java
@@ -1,54 +1,117 @@
 package dao;
 
-import modelo.Fornecedor;
-import modelo.Produto;
-import modelo.Mercadoria;
-import modelo.Servico;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
+import modelo.Produto;
+import modelo.Fornecedor;
+import modelo.Entrada;
 
 public class EntradaDAO {
 
-    public List<Produto> buscar(Fornecedor fornecedor) {
-        int fornecedorId = fornecedor.getId();
-        List<Produto> produtos = new ArrayList<>();
-        String sql = "SELECT * FROM tb_produto WHERE tb_produto.fornecedor_id = ?";
+    private final Connection conexao = new Conexao().getConexao();
 
-        try (Connection conn = Conexao.getConexao();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setInt(1, fornecedorId);
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    String tipo = rs.getString("tipo_produto");
-                    Produto p;
-                    if ("M".equals(tipo)) {
-                        p = new Mercadoria();
-                        ((Mercadoria) p).setPorcentagemMinima(rs.getDouble("porcentagem_minima"));
-                        ((Mercadoria) p).setPorcentagemLucro(rs.getDouble("porcentagem_lucro"));
-                        ((Mercadoria) p).setPerecivel(rs.getBoolean("perecivel"));
-                    } else {
-                        p = new Servico();
-                        ((Servico) p).setGarantia(rs.getString("garantia"));
-                    }
-                    
-                    p.setId(rs.getInt("id"));
-                    p.setNome(rs.getString("nome"));
-                    p.setDescricao(rs.getString("descricao"));
-                    p.setCriadoEm(rs.getDate("criado_em"));
-                    p.setAtualizadoEm(rs.getDate("atualizado_em"));
-                    p.setTipoProduto(tipo);
-                    p.setStatus(rs.getBoolean("status"));
-                    produtos.add(p);
-                }
+    public boolean alterarStatus(Entrada entrada) {
+        String sqlSelect = "SELECT * FROM tb_entrada WHERE id = ?";
+        String sqlUpdate = "UPDATE tb_entrada SET status = ? WHERE id = ?";
+
+        try {
+            PreparedStatement stmt = this.conexao.prepareStatement(sqlSelect);
+            stmt.setInt(1, entrada.getId());
+
+            ResultSet rs = stmt.executeQuery();
+
+            int status = rs.getInt("status");
+
+            stmt = this.conexao.prepareStatement(sqlUpdate);
+            if (status == 1) {
+                stmt.setInt(1, 0);
+            } else {
+                stmt.setInt(1, 1);
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+            stmt.setInt(2, entrada.getId());
+
+            stmt.executeUpdate();
+            stmt.close();
+
+            return true;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return false;
         }
-        return produtos;
+    }
+
+    public ArrayList<Entrada> buscar(Produto produto) {
+        String sql = "SELECT * FROM tb_entrada WHERE produto_id=?";
+
+        try {
+            PreparedStatement stmt = this.conexao.prepareStatement(sql);
+            stmt.setInt(1, produto.getId());
+
+            ResultSet rs = stmt.executeQuery();
+            ArrayList<Entrada> entradas = new ArrayList<>();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                Date data = rs.getDate("data");
+                int quantidade = rs.getInt("quantidade");
+                double precoCusto = rs.getDouble("preco_custo");
+                String lote = rs.getString("lote");
+                Date validade = rs.getDate("validade");
+
+                int id_fornecedor = rs.getInt("fornecedor_id");
+
+                FornecedorDao fornecedorDao = new FornecedorDao();
+                Fornecedor fornecedor = fornecedorDao.buscar(id_fornecedor, 1);
+
+                Entrada entrada = new Entrada(id, data, quantidade, precoCusto, produto, fornecedor, lote, validade);
+
+                entradas.add(entrada);
+            }
+
+            rs.close();
+            stmt.close();
+            return entradas;
+
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+    }
+    
+    public ArrayList<Entrada> buscar(Fornecedor fornecedor) {
+        int fornecedorId = fornecedor.getId();
+        String sql = "SELECT * FROM tb_entrada WHERE tb_entrada.fornecedor_id = ?";
+
+        try {
+            PreparedStatement stmt = this.conexao.prepareStatement(sql);
+            stmt.setInt(1, fornecedor.getId());
+
+            ResultSet rs = stmt.executeQuery();
+            ArrayList<Entrada> entradas = new ArrayList<>();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                Date data = rs.getDate("data");
+                int quantidade = rs.getInt("quantidade");
+                double precoCusto = rs.getDouble("preco_custo");
+                String lote = rs.getString("lote");
+                Date validade = rs.getDate("validade");
+                int id_fornecedor = rs.getInt("fornecedor_id");
+
+                Entrada entrada = new Entrada(id, data, quantidade, precoCusto, produto, fornecedor, lote, validade);
+
+                entradas.add(entrada);
+            }
+
+            rs.close();
+            stmt.close();
+            return entradas;
+
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 }
