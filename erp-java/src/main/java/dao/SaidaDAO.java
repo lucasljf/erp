d3fa3a88ef278259d977fa6464f4443161ef0890
kaickbmc <kaick.costa@estudante.ifgoiano.edu.br>
2024-04-02 15:33:31 -0300

    Cria o método buscar pelo o tipo de saída
    Foi criado e desenvolvido um dos métodos de busca que analisa o tipo de saída e se está ativado ou não e retorna uma lista com as saídas encontradas.

--- erp-java/src/main/java/dao/SaidaDAO.java
+++ erp-java/src/main/java/dao/SaidaDAO.java
@@ -1,6 +1,44 @@
-
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Saida;
+
 public class SaidaDAO {
+    
+    private Connection conexao = new Conexao().getConexao();
+    
+    public List<Saida> buscar(Saida tipoSaida, boolean status) {
+        
+        String nome_tipoSaida = tipoSaida.getTipoSaida();
+        int status_boolean = status ? 1 : 0;
+        
+        List<Saida> saidaEncontrada = new ArrayList<Saida>();
+        
+        String sql = "SELECT * FROM tb_saida WHERE tipo_saida LIKE '%?%' AND status = ?"; 
+        
+        try {
+            PreparedStatement stmt = this.conexao.prepareStatement(sql);       
+            
+            stmt.setString(1, tipoSaida.getTipoSaida());
+            stmt.setBoolean(2, status);
+            stmt.execute();
+            
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Saida saida = new Saida();
+                saida.setTipoSaida(rs.getString("tipo_saida"));
+                saidaEncontrada.add(saida);
+            }
+            return saidaEncontrada;
+            
+        } catch (SQLException e) {
+            return saidaEncontrada;
+        }
+    }
 
 }
