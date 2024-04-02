647e88b688ad003657ba247daa0bef031e40791f
Sara Farias <fariassara012@gmail.com>
2024-04-01 22:14:50 -0300

    Correção do método autenticar

--- erp-java/src/main/java/dao/UsuarioDAO.java
+++ erp-java/src/main/java/dao/UsuarioDAO.java
@@ -12,29 +12,33 @@ public class UsuarioDAO {    //  login e senha
     
     private final Connection conexao = new Conexao().getConexao();
     
-    public Usuario autenticar(Usuario usuario){
+    public Usuario autenticar(String login, String senha){
         
         String sql = "select * from tb_usuario where login = ? and senha = ?";
+        Usuario usuario = new Usuario();
         
         try {
             PreparedStatement stmt = this.conexao.prepareStatement(sql);
-            stmt.setString(1, login.getLogin());
-            stmt.setString(2, senha.getSenha());
+            stmt.setString(1, login);
+            stmt.setString(2, senha);
             
             ResultSet rs = stmt.executeQuery();
-
-            stmt.execute();
-            stmt.close();
             
+            while (rs.next()){
+                String nome = rs.getString("nome");
+                
+                usuario.setNome(nome);
+                usuario.setLogin(login);
+                usuario.setSenha(senha);
+                stmt.close(); 
+            }
             
-
+            return usuario;
+            
         } catch (Exception e) {
             throw new RuntimeException();
-            
         }
-        return usuario;
-        
-        
+       
     }
         
         
