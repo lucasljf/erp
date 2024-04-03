77d654f83b9517f63a84eed3295fa217b11d94cc
joy_c1 <joyce.freitas@estudante.ifgoiano.edu.br>
2024-04-03 15:50:41 -0300

    Criação do teste alterarStatus(Entrada)
    Teste retornando false, foi identificado um probleminha no método alterarStatus(Entrada).
    Além disso, foi notada a ausência do atributo produto: Produto na classe modelo de Entrada.

---
+++ erp-java/src/test/java/dao/EntradaDAOTest.java
@@ -0,0 +1,27 @@
+package dao;
+
+import java.util.Date;
+import modelo.Entrada;
+import modelo.Fornecedor;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EntradaDAOTest {
+
+    @Test
+    public void testAlterarStatus() {
+        
+        //preparação
+        Fornecedor fornecedor = new Fornecedor();        
+        Entrada e1 = new Entrada(new Date(2024, 01, 03), 1, 12.5, fornecedor, "JB2334DDJFRR5", new Date(2024, 02, 01));
+        EntradaDAO dao = new EntradaDAO();
+        
+        e1.setId(1);
+        
+        //ação
+        Boolean statusAlterado = dao.alterarStatus(e1);
+        
+        assertTrue(statusAlterado);
+        
+    }
+}
