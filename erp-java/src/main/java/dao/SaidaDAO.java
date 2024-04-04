2aafe7e2a46fc7c9e5de1f6cc6646dd85cf060fe
kaickbmc <kaick.costa@estudante.ifgoiano.edu.br>
2024-04-03 21:55:21 -0300

    Alterações Necessárias
    Foi realizada umas alterações de detalhas como status que ainda não era necessário nesse momento

--- erp-java/src/main/java/dao/SaidaDAO.java
+++ erp-java/src/main/java/dao/SaidaDAO.java
@@ -5,39 +5,54 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
+import modelo.Produto;
 import modelo.Saida;
 
 public class SaidaDAO {
     
     private Connection conexao = new Conexao().getConexao();
     
-    public List<Saida> buscar(Saida tipoSaida, boolean status) {
+    public ArrayList<Saida> buscar(Saida tipoSaida) {
         
-        String nome_tipoSaida = tipoSaida.getTipoSaida();
-        int status_boolean = status ? 1 : 0;
         
-        List<Saida> saidaEncontrada = new ArrayList<Saida>();
         
-        String sql = "SELECT * FROM tb_saida WHERE tipo_saida LIKE '%?%' AND status = ?"; 
+        String sql = "SELECT * FROM tb_saida WHERE tipo_saida LIKE '%?%'"; 
         
         try {
             PreparedStatement stmt = this.conexao.prepareStatement(sql);       
             
             stmt.setString(1, tipoSaida.getTipoSaida());
-            stmt.setBoolean(2, status);
+
             stmt.execute();
             
             ResultSet rs = stmt.executeQuery();
+            ArrayList<Saida> saidaEncontrada = new ArrayList<>();
+            
             while (rs.next()) {
                 Saida saida = new Saida();
+                int id = rs.getInt("id");
+                
+                int id_produto = rs.getInt("produto_id");
+                
+                ProdutoDao produtoDao = new ProdutoDao();
+                Produto produto = produtoDao.buscar(id_produto, 1)
+                        
+                Date data = rs.getDate("data");
+                
+                double desconto = rs.getDouble("desconto");
+                                
                 saida.setTipoSaida(rs.getString("tipo_saida"));
                 saidaEncontrada.add(saida);
+                
+                saidaEncontrada.add(saida);
             }
+            rs.close();
+            stmt.close();
             return saidaEncontrada;
             
         } catch (SQLException e) {
-            return saidaEncontrada;
+            throw new RuntimeException();
         }
     }
 
