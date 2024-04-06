bb4f47c068e72a8b6a1c6e2e0ca4a75e8e44e256
yaszsz <yasmim.pereira@estudante.ifgoiano.edu.br>
2024-04-05 21:53:28 -0300

    Correções no método buscar(inicio, fim):
    Correções nos parâmetros e nas composições presentes nos objetos do tipo Entrada retornados

--- erp-java/src/main/java/dao/EntradaDAO.java
+++ erp-java/src/main/java/dao/EntradaDAO.java
@@ -1,49 +1,58 @@
-
 package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import modelo.Entrada;
+import modelo.Fornecedor;
+import modelo.Produto;
 
 public class EntradaDAO {
     
    private Connection conexao = new Conexao().getConexao();
    
-    public List<Entrada> buscar(String inicio, String fim){
-    List<Entrada> entradasEncontradas = new ArrayList<>();
+	public List<Entrada> buscar(Date inicio, Date fim){
+	List<Entrada> entradasEncontradas = new ArrayList<>();
 
-    String sql = "SELECT * FROM tb_entrada WHERE data BETWEEN ? AND ?";
+	String sql = "SELECT * FROM tb_entrada WHERE data BETWEEN ? AND ?";
 
     
-        try {
-            PreparedStatement stmt = this.conexao.prepareStatement(sql);
-            stmt.setString(1, inicio);
-            stmt.setString(2, fim);
-            
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                Entrada entrada = new Entrada();
-                entrada.setId(rs.getInt("id"));
-                entrada.setData(rs.getString("data"));
-                entrada.setQuantidade(rs.getInt("quantidade"));
-                entrada.setPrecoCusto(rs.getDouble("preco_custo"));
-                entrada.setLote(rs.getString("lote"));
-                entrada.setProdutoId(rs.getInt("produto_id"));
-                entrada.setValidade(rs.getString("validade"));
-                entrada.setFornecedorId(rs.getInt("fornecedor_id"));
-                entrada.setStatus(rs.getInt("status"));
+    	try {
+        	PreparedStatement stmt = this.conexao.prepareStatement(sql);
+                java.sql.Date inicioSql = new java.sql.Date(inicio.getTime());
+                java.sql.Date fimSql = new java.sql.Date(fim.getTime());
                 
-                entradasEncontradas.add(entrada);
-            }
-        } catch(Exception e) {
-            throw new RuntimeException();
+        	stmt.setDate(1,  inicioSql);
+        	stmt.setDate(2, fimSql);
+       	 
+        	ResultSet rs = stmt.executeQuery();
+        	while (rs.next()) {
+                Date data = rs.getDate("data");
+                int quantidade = rs.getInt("quantidade");
+                double precoCusto = rs.getDouble("preco_custo");
+                String lote = rs.getString("lote");
+                Date validade = rs.getDate("validade");
+
+                int idFornecedor = rs.getInt("fornecedor_id");
+                FornecedorDAO fornecedorDao = new FornecedorDAO();
+                Fornecedor fornecedor = fornecedorDao.buscar(idFornecedor, true);
+                
+                int idProduto = rs.getInt("produto_id");
+                ProdutoDAO produtoDao = new ProdutoDAO();
+                Produto produto = produtoDao.buscar(idProduto, true);                
+
+                Entrada e = new Entrada(data, quantidade, precoCusto, fornecedor, produto, lote, validade);
+
+            	entradasEncontradas.add(e);
+        	}
+                
+    	} catch(Exception e) {
+            e.printStackTrace();
         }
-        
-        return entradasEncontradas;
-    }
+   	 
+    	return entradasEncontradas;
+	}
 }
-
-
