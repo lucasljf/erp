b394b581b90a28a4ba75df0c63f94d0427eed4e9
cmarialaura <marialauraschaves@gmail.com>
2024-04-10 16:20:35 -0300

    Método salvar(saida) saidadao

--- erp-java/src/main/java/dao/SaidaDAO.java
+++ erp-java/src/main/java/dao/SaidaDAO.java
@@ -1,6 +1,25 @@
-
 package dao;
 
-public class SaidaDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Saida;
 
+public class SaidaDAO {
+    private list<Saida> listaSaidas;
+
+    public SaidaDAO() {
+        this.listaSaidas = new Arraylist<>();
+    }
+    
+    //Método para buscar saída
+    
+    public void salvar(Saida saida){
+        listaSaidas.add(saida);
+        System.out.println("Saída salva com sucesso");
+    }
 }
+
