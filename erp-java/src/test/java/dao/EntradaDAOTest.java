3c5f50f7fa030975e157eaf1c7c30b17e37ea83d
joy_c1 <joyce.freitas@estudante.ifgoiano.edu.br>
2024-04-11 09:20:25 -0300

    Correção no construtor de Entrada
    Instancia um Produto que faltava para compor a Entrada

--- erp-java/src/test/java/dao/EntradaDAOTest.java
+++ erp-java/src/test/java/dao/EntradaDAOTest.java
@@ -3,6 +3,8 @@ package dao;
 import java.util.Date;
 import modelo.Entrada;
 import modelo.Fornecedor;
+import modelo.Produto;
+import modelo.Servico;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,8 +14,9 @@ public class EntradaDAOTest {
     public void testAlterarStatus() {
         
         //preparação
-        Fornecedor fornecedor = new Fornecedor();        
-        Entrada e1 = new Entrada(new Date(2024, 01, 03), 1, 12.5, fornecedor, "JB2334DDJFRR5", new Date(2024, 02, 01));
+        Fornecedor fornecedor = new Fornecedor("Fornecedor", "62985126398", "00.123.456/0001-78", "fornecedor@example.com");
+        Produto produto = new Servico("Produto", "Produto exemplo", new Date(), new Date(), "1 ano");
+        Entrada e1 = new Entrada(new Date(), 1, 12.5, fornecedor, produto, "JB2334DDJFRR5", new Date());
         EntradaDAO dao = new EntradaDAO();
         e1 = dao.salvar(e1);
         
