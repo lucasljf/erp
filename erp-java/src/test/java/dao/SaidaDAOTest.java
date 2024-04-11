ecf21db457a0f5a66279050c0435079d978e913c
Alexandre Neves <alexandre.neves@estudante.ifgoiano.edu.br>
2024-04-11 15:19:07 -0300

    Fix teste buscar(tipo) SaidaDAO
    Foi adicionado a inserção das saídas no banco para então testar o método, assim como foi comentado na última aula.

--- erp-java/src/test/java/dao/SaidaDAOTest.java
+++ erp-java/src/test/java/dao/SaidaDAOTest.java
@@ -1,30 +1,48 @@
 package dao;
 
 import modelo.Saida;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
+import java.util.Date;
+import modelo.Mercadoria;
+import modelo.Produto;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 class SaidaDAOTest {
+    
     @Test
-    @DisplayName("Verifica se o resultado das buscas não é vazio")
-    public void testBuscar(){
-        //Buscar por tipo de saída (Venda, perca, outros).
+    public void testBuscar() {
+        Date data = new Date();
+        Mercadoria produto = new Mercadoria("Coca-Cola",
+                "Refrigerante lata 350ml",
+                data, data, 12D, 30D, true
+        );
+        ProdutoDAO produtoDAO = new ProdutoDAO();
+        
+        //pegando o Id do produto persistido
+        produto = produtoDAO.salvar(produto);
+        
         SaidaDAO saidaDAO = new SaidaDAO();
+        ArrayList<Saida> saidas = new ArrayList<>();
+        
+        Saida saidaVenda = new Saida(0,produto, data, 0, "venda");
+        Saida saidaPerca = new Saida(0,produto, data, 0, "perca");
+        Saida saidaOutros = new Saida(0,produto, data, 0, "outros");
+        
+        saidaDAO.salvar(saidaVenda);
+        saidas.addAll(saidaDAO.buscar("venda"));
+        
 
-        ArrayList<Saida> saidasVenda = new ArrayList<>();
-        saidasVenda = saidaDAO.buscar("venda");
-        assertFalse(saidasVenda.isEmpty());
-
-        ArrayList<Saida> saidasPerca = new ArrayList<>();
-        saidasPerca = saidaDAO.buscar("perca");
-        assertFalse(saidasPerca.isEmpty());
-
-        ArrayList<Saida> saidasOutros = new ArrayList<>();
-        saidasVenda = saidaDAO.buscar("outros");
-        assertFalse(saidasVenda.isEmpty());
+        saidaDAO.salvar(saidaPerca);
+        saidas.addAll(saidaDAO.buscar("perca"));
+        
+        saidaDAO.salvar(saidaOutros);
+        saidas.addAll(saidaDAO.buscar("outros"));
+        
+        assertFalse(saidas.isEmpty());
+        assertEquals(3, saidas.size());
     }
 }
