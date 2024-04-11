af389b1420536366f4bacbb28562df96b8b7b18a
FelipeFidelisA <146492055+FelipeFidelisA@users.noreply.github.com>
2024-04-10 21:16:03 -0300

    Correções

--- erp-java/src/main/java/dao/EntradaDAO.java
+++ erp-java/src/main/java/dao/EntradaDAO.java
@@ -7,9 +7,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class EntradaDAO {
-
+    
+    
+    
+    private final Connection conexao = new Connection().Conexao.getConexao();
+    
+    
     public Entrada salvar(Entrada entrada) {
+        
         String sql = "insert into tb_entrada ( data, quantidade, preco_custo, lote, produto_id, validade, fornecedor_id, status) values (?,?,?,?,?,?,?,?)";
+
 
         try {
             PreparedStatement ps = this.conexao.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
@@ -18,14 +25,16 @@ public class EntradaDAO {
             ps.setInt(2, entrada.getQuantidade);
             ps.setInt(3, entrada.getPrecoCusto);
             ps.setInt(4, entrada.getLote);
-            ps.setInt(5, entrada.getProdutoId);
+            ps.setInt(5, entrada.getProduto().getId());
             ps.setDate(6, entrada.getValidade);
-            ps.setInt(7, entrada.getFornecedorId);
-            ps.setInt(8, entrada.getStatus);
+            ps.setInt(7, entrada.getFornecedor().getId());
+            ps.setInt(8, 1);
 
+            ps.execute();
+            ResultSet rs = ps.getGeneratedKeys();
             ResultSet rs = ps.executeQuery();
 
-            entrada.setId(rs.getInt("1"));
+            entrada.setId(rs.getInt(1));
 
             return entrada;
         } catch (SQLException exception) {
