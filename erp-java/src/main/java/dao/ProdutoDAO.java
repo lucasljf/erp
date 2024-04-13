198610d90048e7dc0b23358e9fc19fdd85817b79
SamuelNM <samuel17nunesmartins@gmail.com>
2024-04-13 20:25:38 -0300

    correção metodo salvar(Produto)

--- erp-java/src/main/java/dao/ProdutoDAO.java
+++ erp-java/src/main/java/dao/ProdutoDAO.java
@@ -1,4 +1,3 @@
-
 package dao;
 
 import java.sql.Connection;
@@ -10,49 +9,52 @@ import modelo.Produto;
 import modelo.Servico;
 
 public class ProdutoDAO {
+
     private final Connection conexao = new Conexao().getConexao();
-    
+
     public Produto salvar(Produto produto) {
-        String sqlSalvarProduto = "INSERT INTO tb_produto (nome, descricao, criado_em, atualizado_em) VALUES (?, ?, ?, ?)";
-        
+
         try {
-            PreparedStatement stmt = this.conexao.prepareStatement(sqlSalvarProduto, PreparedStatement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, produto.getNome());
-            stmt.setString(2, produto.getDescricao());
-            stmt.setDate(3, new java.sql.Date(produto.getCriadoEm().getTime()));
-            stmt.setDate(4, new java.sql.Date(produto.getAtualizadoEm().getTime()));
-            
-            stmt.execute();
-            
-            ResultSet rs = stmt.getGeneratedKeys();
-            
-            produto.setId(rs.getInt(1));
-            
-            if(produto instanceof Mercadoria){
+            if (produto instanceof Mercadoria) {
                 Mercadoria mercadoria = (Mercadoria) produto;
-                
-                String sqlSalvarMercadoria = "UPDATE tb_produto SET quantidade_minima = ?, porcentagem_lucro = ?, perecivel = ? WHERE id = ?";
-                
-                PreparedStatement stmtMercadoria = this.conexao.prepareStatement(sqlSalvarMercadoria);
-                stmtMercadoria.setDouble(1, mercadoria.getQuantidadeMinima());
-                stmtMercadoria.setDouble(2, mercadoria.getPorcentagemLucro());
-                stmtMercadoria.setBoolean(3, mercadoria.isPerecivel());
-                stmtMercadoria.setInt(4, mercadoria.getId());
-                
-                stmtMercadoria.executeUpdate();
-                
-            }else if(produto instanceof Servico){
+
+                String sqlSalvarMercadoria = "INSERT INTO tb_produto (nome, descricao, criado_em, atualizado_em, quantidade_minima, porcentagem_lucro, perecivel, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+                PreparedStatement stmt = this.conexao.prepareStatement(sqlSalvarMercadoria, PreparedStatement.RETURN_GENERATED_KEYS);
+                stmt.setString(1, mercadoria.getNome());
+                stmt.setString(2, mercadoria.getDescricao());
+                stmt.setDate(3, new java.sql.Date(mercadoria.getCriadoEm().getTime()));
+                stmt.setDate(4, new java.sql.Date(mercadoria.getAtualizadoEm().getTime()));
+                stmt.setDouble(5, mercadoria.getQuantidadeMinima());
+                stmt.setDouble(6, mercadoria.getPorcentagemLucro());
+                stmt.setBoolean(7, mercadoria.isPerecivel());
+                stmt.setObject(8, mercadoria.getStatus());
+
+                stmt.execute();
+
+                ResultSet rs = stmt.getGeneratedKeys();
+
+                produto.setId(rs.getInt(1));
+
+            } else if (produto instanceof Servico) {
                 Servico servico = (Servico) produto;
-                
-                String sqlSalvarServico = "UPDATE tb_produto SET garantia = ? WHERE id = ?";
-                
-                PreparedStatement stmtServico = this.conexao.prepareStatement(sqlSalvarServico);
-                stmtServico.setString(1, servico.getGarantia());
-                stmtServico.setInt(2, servico.getId());
-                
-                stmt.executeUpdate();
+
+                String sqlSalvarServico = "INSERT INTO tb_produto (nome, descricao, criado_em, atualizado_em, garantia) VALUES (?, ?, ?, ?, ?)";
+
+                PreparedStatement stmt = this.conexao.prepareStatement(sqlSalvarServico);
+                stmt.setString(1, servico.getNome());
+                stmt.setString(2, servico.getDescricao());
+                stmt.setDate(3, new java.sql.Date(servico.getCriadoEm().getTime()));
+                stmt.setDate(4, new java.sql.Date(servico.getAtualizadoEm().getTime()));
+                stmt.setString(5, servico.getGarantia());
+
+                stmt.execute();
+
+                ResultSet rs = stmt.getGeneratedKeys();
+
+                produto.setId(rs.getInt(1));
             }
-            
+
             return produto;
         } catch (SQLException e) {
             throw new RuntimeException();
