08176b4012129033320c22f61f3ee56e110b02c5
hugosymon <symondonega@gmail.com>
2024-07-03 20:55:50 -0300

    Correcao teste metodo buscar produto

--- erp-java/src/main/java/dao/SaidaDAO.java
+++ erp-java/src/main/java/dao/SaidaDAO.java
@@ -2,10 +2,7 @@ package dao;
 
 import modelo.Produto;
 import modelo.Saida;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,9 +12,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.*;
+
 
 public class SaidaDAO {
 
@@ -153,41 +148,5 @@ public class SaidaDAO {
             exception.printStackTrace();
             return false;
         }
-    }
-}
-public class SaidaDAOTeste{
-    public Connection mockConnettion;
-    public Preparedstatement mockPreparedstatement;
-    public Resultset mockResultset;
-    public SaidaDAO saidaDAO;
-
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        saidaDAO = new SaidaDAO();
-        saidaDAO.conexao = mockConnection;
-    }
-    public void testBuscarProduto() throws SQLException {
-
-        Produto produto = new Produto();
-
-        produto.setId(1);
-        when(mockConnection.prepareStatement(anyString())).thenReturn(mockPreparedStatement);
-        when(mockPreparedStatement.executeQuery()).thenReturn(mockResultSet);
-
-        when(mockResultSet.next()).thenReturn(true).thenReturn(false);
-        when(mockResultSet.getInt(1)).thenReturn(1);
-        when(mockResultSet.getDate(2)).thenReturn(new java.sql.Date(System.currentTimeMillis()));
-        when(mockResultSet.getDouble(4)).thenReturn(10.0);
-        when(mockResultSet.getNString(5)).thenReturn("saìda");
-
-        ArrayList<Saida> saidas = saidaDAO.buscar(produto);
-
-        assertNotNull(saidas);
-        assertEquals(1, saidas.size());
-        Saida saida = saidas.get(0);
-        assertEquals(1, saida.getId());
-        assertEquals(produto, saida.getProduto());
-        assertEquals(10.0, saida.getDesconto());
-        assertEquals("saida", saida.getTipoSaida());
     }
 }
