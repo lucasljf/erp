ffa755f5dd69e34e837a65b5df10bab141a1f275
guiAlencar <uchihaguilherme4@gmail.com>
2024-07-15 18:23:22 -0300

    adiciona cadastrar servico
    Funcionalidade do botão cadastrar serviço

--- erp-java/src/main/java/controlador/ServicoController.java
+++ erp-java/src/main/java/controlador/ServicoController.java
@@ -1,6 +1,32 @@
 
 package controlador;
 
-public class ServicoController {
+import dao.ProdutoDAO;
+import java.util.Date;
+import modelo.Produto;
+import modelo.Servico;
 
+public class ServicoController {
+    public boolean cadastrarServico(String nome, String descricao, String garantia) {
+        //fazer aqui
+        //buscar por nome
+        Servico servico = new Servico(nome, descricao, new Date(), new Date(), garantia);
+        ProdutoDAO produtoDao = new ProdutoDAO();
+        //criar validação de nome
+        
+        //validacao campos vazios
+        if (nome == null || nome.isEmpty() || descricao == null || descricao.isEmpty() || garantia == null || garantia.isEmpty()) {
+            return false;
+        }
+        //Servico servico2 = produtoDao.salvar(servico);
+        Servico servico2 = (Servico) produtoDao.salvar(servico);
+        System.out.println(servico2.getId());
+        //return true;
+        
+        if (servico2 == null) {
+          return false;
+        } else {
+          return true;
+        }
+    }
 }
