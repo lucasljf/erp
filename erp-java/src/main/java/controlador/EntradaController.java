941c08e3bb9cd239aa06cbd1fdd986e28f0c815d
MarioALves2002 <marioalves5627@gmail.com>
2024-07-17 17:08:29 -0300

    atualiza entradaController

--- erp-java/src/main/java/controlador/EntradaController.java
+++ erp-java/src/main/java/controlador/EntradaController.java
@@ -1,16 +1,15 @@
 package controlador;
 
-import java.util.Date;
 import modelo.Entrada;
 import modelo.Fornecedor;
 import modelo.Produto;
+import java.util.Date;
 
 public class EntradaController {
 
     public boolean cadastrarEntrada(Produto produto, Date data, int quantidade, double precoCusto, Fornecedor fornecedor, String lote, Date validade) {
         if (fornecedor != null && lote != null && validade != null && produto != null) {
             Entrada entrada = new Entrada(produto, data, quantidade, precoCusto, fornecedor, lote, validade);
-
             return true;
         }
         return false;
