930d3adeba5d4510041eeac11a36596e54c425a6
jhannyfer <jhannyferbiangulo@gmail.com>
2024-07-26 01:05:35 -0300

    Criando Controller
    Criação do Controller para buscar fornecedores

--- erp-java/src/main/java/controlador/FornecedorController.java
+++ erp-java/src/main/java/controlador/FornecedorController.java
@@ -1,6 +1,18 @@
-
 package controlador;
 
+import dao.FornecedorDAO;
+import java.util.List;
+import modelo.Fornecedor;
+
 public class FornecedorController {
+    private FornecedorDAO fornecedorDAO;
+
+    public FornecedorController() {
+        this.fornecedorDAO = new FornecedorDAO();
+    }
+
+    public List<Fornecedor> buscar(String nome, boolean status) {
+        return fornecedorDAO.buscar(nome, status);
+    }
 
 }
