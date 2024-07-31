e14c3e0f03b3b7748d261b4c5384c0a96a6ebce0
Luis Fernando de Oliveira Naves <luis123fernandonaves@gmail.com>
2024-07-30 22:07:24 -0300

    Criado método preencherLista
    Criado método preencherLista para preencher a lista de saídas.
    
    O método necessita da criação de um método buscarTodas no SaidaDAO.

--- erp-java/src/main/java/controlador/SaidaController.java
+++ erp-java/src/main/java/controlador/SaidaController.java
@@ -1,6 +1,19 @@
 
 package controlador;
 
+import dao.SaidaDAO;
+import modelo.Saida;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SaidaController {
 
+    public List<Saida> preencherLista() {
+        List<Saida> listaSaidas = new ArrayList<>();
+        SaidaDAO saidaDAO = new SaidaDAO();
+
+        listaSaidas = saidaDAO.buscarTodas();
+        
+        return listaSaidas;
+    }
 }
