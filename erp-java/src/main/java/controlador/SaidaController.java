15bf6ddeb9c19223431b50216d5fc1d4166f11fc
Luis Fernando de Oliveira Naves <luis123fernandonaves@gmail.com>
2024-08-04 16:45:59 -0300

    Atualizando exibição da lista de saída
    Atualizado para buscar todos as saídas e exibi-las assim que iniciar a tela.

--- erp-java/src/main/java/controlador/SaidaController.java
+++ erp-java/src/main/java/controlador/SaidaController.java
@@ -8,12 +8,19 @@ import java.util.List;
 
 public class SaidaController {
 
-    public List<Saida> preencherLista() {
-        List<Saida> listaSaidas = new ArrayList<>();
+    public List<Object[]> preencherTabela() {
+        List<Object[]> dadosTabela = new ArrayList<>();
         SaidaDAO saidaDAO = new SaidaDAO();
 
-        listaSaidas = saidaDAO.buscarTodas();
-        
-        return listaSaidas;
+        List<Saida> listaSaidas = new ArrayList<>();
+        listaSaidas.addAll(saidaDAO.buscar("Venda"));
+        listaSaidas.addAll(saidaDAO.buscar("Perca"));
+
+        for (Saida saida : listaSaidas) {
+            Object[] linha = {saida.getProduto().getNome(), saida.getTipoSaida(), saida.getData()};
+            dadosTabela.add(linha);
+        }
+
+        return dadosTabela;
     }
 }
