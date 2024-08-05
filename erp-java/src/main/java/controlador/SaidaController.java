24868f0aaaf594257c47520e5f5e0fb5512df199
Luis Fernando de Oliveira Naves <luis123fernandonaves@gmail.com>
2024-08-05 17:12:29 -0300

    Criando actionPerformed do botão buscar
    Fiz algumas atualizações na view, como mudar a posição de alguns campos e adicionar mais dois botão do tipo radio.
    
    E o principal é o botão de buscar que agora chama o método de buscar os filtros(Método da issue Saída: Filtrar Saída#167).
    
    SaidaController.java esta vazia pois a minha issue é de fazer a ligação do método com a view, e no commit passado eu tentei criar o método sem querer :see_no_evil: .

--- erp-java/src/main/java/controlador/SaidaController.java
+++ erp-java/src/main/java/controlador/SaidaController.java
@@ -1,41 +1,6 @@
 
 package controlador;
 
-import dao.SaidaDAO;
-import dao.ProdutoDAO;
-import modelo.Saida;
-import modelo.Produto;
-import java.util.ArrayList;
-import java.util.List;
-
-public class SaidaController {   
+public class SaidaController {
     
-    public List<Saida> buscarPorFiltros(String nome, String tipoFiltro, String tipoFiltroTexto, Date dataInicio, Date dataFim) {
-        
-        List<Saida> listaFiltrada = new ArrayList<>();
-        SaidaDAO saidaDAO = new SaidaDAO();
-        ProdutoDAO produtoDAO = new ProdutoDAO();
-
-        switch (tipoFiltro) {
-            case "Produto":
-                ArrayList<Produto> produtos = produtoDAO.buscar(nome, true);
-                if (produtos != null) {
-                    for (Produto produto : produtos) {
-                        listaFiltrada.addAll(saidaDAO.buscar(produto));
-                    }
-                }
-                break;
-            case "Tipo":
-                listaFiltrada = saidaDAO.buscar(tipoFiltroTexto);
-                break;
-            case "Data":
-                listaFiltrada = saidaDAO.buscar(dataInicio, dataFim);
-                break;
-            default:
-                listaFiltrada = saidaDAO.buscarTodas();
-                break;
-        }
-        
-        return listaFiltrada;
-    }
 }
