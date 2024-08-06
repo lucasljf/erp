9746c39d27c7e5a313f3214fd88e40e4458227f0
Emiliano Souza <141650523+Emiliano-Souza@users.noreply.github.com>
2024-08-06 16:49:05 -0300

    Update EntradaController.java
    add controller da tela BuscarEntrada. 
    necessitada de busca por String que devolva apenas 1 objeto para Produto e Fornecedor, os metodos existentes retornam arrays.

--- erp-java/src/main/java/controlador/EntradaController.java
+++ erp-java/src/main/java/controlador/EntradaController.java
@@ -1,6 +1,58 @@
-
 package controlador;
 
-public class EntradaController {
+import dao.EntradaDAO;
+import dao.FornecedorDAO;
+import dao.ProdutoDAO;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import modelo.Entrada;
+import modelo.Fornecedor;
+import modelo.Produto;
 
+public class EntradaController {
+    
+    public List<Entrada> BuscarEntrada(String strFornecedor, Date inicio, Date fim) {
+        
+        FornecedorDAO fornecedorDao = new FornecedorDAO();
+        Fornecedor fornecedor = fornecedorDao.buscar(strFornecedor);
+        
+        List<Entrada> buscasEntrada = new ArrayList();
+        EntradaDAO entradaDAO = new EntradaDAO();
+
+        List<Entrada> entradas = entradaDAO.buscar(fornecedor);
+
+        for (Entrada entrada : entradas) {
+            Date dataEntrada = entrada.getData();
+            if (dataEntrada.after(inicio) && dataEntrada.before(fim)) {
+                buscasEntrada.add(entrada);
+            }
+        }
+
+        return buscasEntrada;
+    }
+    
+    public List<Entrada> BuscarEntrada(String strFornecedor, String strProduto, Date inicio, Date fim) {
+        
+        FornecedorDAO fornecedorDao = new FornecedorDAO();
+        Fornecedor fornecedor = fornecedorDao.buscar(strFornecedor);
+        
+        ProdutoDAO produtoDao = new ProdutoDAO();
+        Produto produto = produtoDao.buscar(strProduto);
+        
+        List<Entrada> buscasEntrada = new ArrayList<>();
+        EntradaDAO entradaDAO = new EntradaDAO();
+
+        List<Entrada> entradas = entradaDAO.buscar(fornecedor);
+
+        for (Entrada entrada : entradas) {
+            Date dataEntrada = entrada.getData();
+            Produto produtoEntrada = entrada.getProduto();
+            if (dataEntrada.after(inicio) && dataEntrada.before(fim) && produto == produtoEntrada) {
+                buscasEntrada.add(entrada);
+            }
+        }
+
+        return buscasEntrada;
+    }
 }
